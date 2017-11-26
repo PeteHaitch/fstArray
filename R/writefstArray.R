6ea3b94bae1de0095e52b8199a7030fed3fdff10
Peter Hickey <peter.hickey@gmail.com>
2017-11-25 23:12:58 -0500

    writefstArray() can only handle 2-d arrays

--- R/writefstArray.R
+++ R/writefstArray.R
@@ -7,6 +7,10 @@
 #' @importFrom fst write.fst
 #' @export
 writefstArray <- function(x, file, compress = 0) {
+  dim_x <- dim(x)
+  if (length(dim_x) != 2L) {
+    stop("'x' must be 2-dimensional")
+  }
   x <- as.data.frame(x)
   write.fst(x, path = file, compress = compress)
   fstArray(file)
